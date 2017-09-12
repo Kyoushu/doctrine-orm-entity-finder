e1ec0d8857de577bdd2c7cf84ee9a93d147d9708
James Willans <james.willans@accordmarketing.com>
2017-09-12 10:15:58 +0100

    Fixed bootstrap to allow unit tests to run

--- src/Kyoushu/DoctrineORMEntityFinder/Test/bootstrap.php
+++ src/Kyoushu/DoctrineORMEntityFinder/Test/bootstrap.php
@@ -1,4 +1,10 @@
 <?php
 
-$loader = require_once(__DIR__ . '/../../../../vendor/autoload.php');
-\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+namespace Kyoushu;
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Composer\Autoload\ClassLoader;
+
+/** @var ClassLoader $loader */
+$loader = require(__DIR__ . '/../../../../vendor/autoload.php');
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
